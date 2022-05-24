7dd55f5f01c5890ad60b746b9e4fc6ab94881e3f
SanM <sanmiao.xu@my.jcu.edu.au>
2022-05-24 15:04:36 +0800

    add a password checking function

---
+++ password_checker.py
@@ -0,0 +1,10 @@
+def is_valid_password(text):
+    return 8 <= len(text) <= 20
+
+
+def main():
+    new_password = "ssssssssss"
+    print(f"{new_password} is a valid password?{is_valid_password(new_password)}")
+
+
+main()
